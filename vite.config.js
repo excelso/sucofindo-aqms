7dac85b87b485efb7317c132c2c0daec39415482
Widi Widayat Y <excelsowidi@gmail.com>
2025-08-08 10:05:13 +0800

    Pembaharuan

--- vite.config.js
+++ vite.config.js
@@ -22,8 +22,9 @@ export default defineConfig({
                 'resources/js/main/dashboard/index.tsx',
                 'resources/js/main/settings/change-password/index.tsx',
                 'resources/js/main/master/data-platform-loggers/index.tsx',
+                'resources/js/main/reports/index.tsx',
                 'resources/js/main/master/data-sites/index.tsx',
-                'resources/js/main/master/data-users/index.tsx'
+                'resources/js/main/master/data-users/index.tsx',
             ],
             refresh: true,
         }),
