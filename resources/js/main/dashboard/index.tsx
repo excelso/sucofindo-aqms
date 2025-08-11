f133064fe7cec091e6b1c69129c642a3f6692f42
Widi Widayat Y <excelsowidi@gmail.com>
2025-08-11 17:20:20 +0700

    Pembaharuan

--- resources/js/main/dashboard/index.tsx
+++ resources/js/main/dashboard/index.tsx
@@ -304,7 +304,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 text: `${metric.title}`
             },
             subtitle: {
-                text: `Range: ${metric.bml_max_buffer} - ${metric.bml_max}`
+                text: `Range: Buffer (${metric.bml_max_buffer}) - BML (${metric.bml_max})`
             },
             credits: {enabled: false},
             xAxis: {
