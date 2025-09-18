e3b438bac3612e5b9be17c38a120ddf3da3802a6
Widi Widayat Y <excelsowidi@gmail.com>
2025-09-18 20:05:52 +0700

    Pembaharuan

--- app/Models/Master/Loggers.php
+++ app/Models/Master/Loggers.php
@@ -117,16 +117,7 @@
                 $builder->selectRaw('ROUND(AVG(pm_25), 0) AS pm_25');
                 $builder->selectRaw('MAX( pm_25 ) AS max_pm_25');
                 $builder->selectRaw('ROUND(AVG(tsp), 0) AS tsp');
-                $builder->selectRaw('
-                    ROUND(FIRST_VALUE(tsp) OVER (
-                        PARTITION BY uid, FLOOR(datetime_unix / 600)
-                        ORDER BY tsp DESC
-                    ), 0) AS max_tsp,
-                    ROUND(FIRST_VALUE(aqi_index_tsp) OVER (
-                        PARTITION BY uid, FLOOR(datetime_unix / 600)
-                        ORDER BY tsp DESC
-                    ), 0) AS max_aqi_index_tsp
-                ');
+                $builder->selectRaw('MAX( tsp ) AS max_tsp');
                 $builder->selectRaw('ROUND(AVG(noise), 2) AS noise');
                 $builder->selectRaw('MAX( noise ) AS max_noise');
                 $builder->selectRaw('ROUND((10 * LOG10((1/count(*) * SUM(POWER(10, noise / 10))))), 2) AS noise_leq');
