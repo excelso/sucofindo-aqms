e220757a3c5dad53354e5e0d237346a0e426e991
Widi Widayat Y <excelsowidi@gmail.com>
2025-11-07 17:40:59 +0700

    Pembaharuan

--- app/Models/BeSparing/Master/PlatformWeeklySummary.php
+++ app/Models/BeSparing/Master/PlatformWeeklySummary.php
@@ -28,7 +28,10 @@
             $builder->whereDate('t_platform_weekly_summary.week_start', '=', $minDate);
             $builder->whereDate('t_platform_weekly_summary.week_until', '=', $maxDate);
 
-            $builder->join('t_platform', 't_platform_weekly_summary.uid', '=', 't_platform.uid');
+            $builder->leftJoin('t_platform', function($join) {
+                $join->on('t_platform_weekly_summary.uid', '=', 't_platform.uid');
+                $join->on('t_platform_weekly_summary.tipe_logger', '=', 't_platform.tipe_logger');
+            });
             $builder->leftJoin('t_customer_site', 't_platform.site_id', '=', 't_customer_site.id');
             $builder->leftJoin('t_customer_lokasi', 't_customer_site.customer_lokasi_id', '=', 't_customer_lokasi.id');
 
